83b0420686f7ec41c7b16dbba67703c66b400e26
Ziad Ahmed <ziad32ahmed111@gmail.com>
2026-08-28 21:39:19 +0300

    Add: reconstruct-flight-path - submission-3

---
+++ Data Structures & Algorithms/reconstruct-flight-path/submission-3.py
@@ -0,0 +1,32 @@
+from collections import defaultdict
+from typing import List
+
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        # using dict create a directed graph
+        graph = defaultdict(list)
+        for src, dst in tickets:
+            graph[src].append(dst)
+            
+        # sort the destinations alphabetically so we process in order
+        for src in graph:
+            graph[src].sort()
+            
+        itinerary = []
+        
+        # DFS to take all destinations in order
+        def dfs(airport):
+            # while there are still out-bound flights from this airport
+            while graph[airport]:
+                # take the first available destination
+                next_dest = graph[airport].pop(0)
+                dfs(next_dest)
+            
+            # append the airport ONLY when it has no more outgoing flights
+            itinerary.append(airport)
+            
+        dfs("JFK")
+        
+        # Since we appended dead-ends first, the itinerary is backwards. 
+        # We reverse it to get the correct chronological flight path.
+        return itinerary[::-1]
